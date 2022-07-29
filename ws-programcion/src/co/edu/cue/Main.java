de96dd8f55eb8c4eba43f130a8c52f9aecdc2ddb
PruebasPA <jandpos@cqteduca.edu.com>
2022-07-29 09:07:03 -0500

    Adelanto

--- ws-programcion/src/co/edu/cue/Main.java
+++ ws-programcion/src/co/edu/cue/Main.java
@@ -1,7 +1,59 @@
 package co.edu.cue;
 
+import javax.swing.*;
+import java.lang.invoke.CallSite;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Objeto>listadeObjetos=new ArrayList<>();
+        Objeto objeto1=new Objeto("1111","Televisor",5,"disponible",30000);
+        Objeto objeto2=new Objeto("2222","Lavadora",3,"disponible",50000);
+        Objeto objeto3=new Objeto("3333","Pc",10,"disponible",45000);
+        listadeObjetos.add(objeto1);
+        listadeObjetos.add(objeto2);
+        listadeObjetos.add(objeto3);
+
+        List<Empleado>listadempleados=new ArrayList<>();
+        Empleado empleado1=new Empleado("1212","Fernando","Fercho@gmail.com",3);
+        Empleado empleado2=new Empleado("1313","Mariana","Marian@gmail.com",2);
+        Empleado empleado3=new Empleado("1414","Lisardo","Lisardo@gmail.com",10);
+        listadempleados.add(empleado1);
+        listadempleados.add(empleado2);
+        listadempleados.add(empleado3);
+
+        List<Cliente>listadeclientes=new ArrayList<>();
+        int opcion = Integer.parseInt(JOptionPane.showInputDialog("Presione "+"\n"+"1"));
+/*        int opcion = Integer.parseInt(JOptionPane.showInputDialog("Que objeto desea prestar"+"\n"+"1: Televisor"+"\n"+"2: Lavadora"+
+                "\n"+"3: Pc"));*/
+        switch (opcion) {
+            case 1:
+                if(listadempleados.size()<3){
+                    String code=JOptionPane.showInputDialog("Ingresa tu codigo");
+                    String nombre=JOptionPane.showInputDialog("Ingresa tu nombre");
+                    String correo=JOptionPane.showInputDialog("Ingresa tu nombre");
+                    int añosant =Integer.parseInt(JOptionPane.showInputDialog("Ingrese cantidad de años"));
+
+                    Empleado empleado=new Empleado(code,nombre,correo,añosant);
+                }
+            case 2:
+                if(listadeclientes.size()<2){
+                    String nombre=JOptionPane.showInputDialog("Ingresa el nombre");
+                    String gender=JOptionPane.showInputDialog("Ingresa F si eres Mujer, M si eres hombre");
+                    String Typedocument=JOptionPane.showInputDialog("Ingresa PP si es Pasaporte o CC si es cedula");
+                    String Document=JOptionPane.showInputDialog("Ingresa tu numero de documento");
+                    String city=JOptionPane.showInputDialog("Ingresa el nombre de tu ciudad");
+
+                    Cliente cliente1 = new Cliente(Document,Typedocument,nombre,gender,city);
+                }
+                else {
+                    JOptionPane.showMessageDialog(null,"lo siento no podemos aceptar mas clientes");
+                }
+            case 3:
+                /*JOptionPane.showMessageDialog();*/
+        }
+        listadempleados.get(0).getCode();
+
     }
 }
